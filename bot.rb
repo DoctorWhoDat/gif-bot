f4e85dcc6905f87841c38472ed0ac9fb8975e325
Charles Jones <charles.jones29@hotmail.com>
2021-03-12 08:54:47 -0600

    feat(add) add do it

--- bot.rb
+++ bot.rb
@@ -9,9 +9,14 @@ bot.message(containing: /o+h+ n+o+/i) do |event|
   event.respond 'https://tenor.com/Uyxd.gif'
 end
 
-bot.message(containing: /u+g+h+/i) do |event|
+bot.message(content: /u+g+h+/i) do |event|
   event.respond 'ughhhhh'
 end
+
+bot.message(containing: /d+o+ i+t+/i) do |event| 
+  event.respond 'https://tenor.com/2Uz8.gif'
+  event.respond 'https://tenor.com/uQj1.gif'
+  event.respond 'https://tenor.com/sMB9.gif'
 
 at_exit { bot.stop }
 bot.run
